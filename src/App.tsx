17035562a9e10707bef192fa1a9ccfa0098878c2
fantasyalebar <bardotalexandre@gmail.com>
2023-11-28 21:48:15 +0100

    v0.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import { questions, Question } from './dataset';
-import Card from './components/Card';
-import ProgressBar from './components/ProgressBar';
-import TestResults from './components/TestResults';
-import { FaPlay, FaPause } from 'react-icons/fa';
-import './App.css';
-import { initializeDragAndDrop } from './dragnDrop';
+import React, { useState, useEffect } from "react";
+import { questions, Question } from "./dataset";
+import Card from "./components/Card";
+import ProgressBar from "./components/ProgressBar";
+import TestResults from "./components/TestResults";
+import { FaPlay, FaPause } from "react-icons/fa";
+import "./App.css";
+import { initializeDragAndDrop } from "./dragnDrop";
 
 const App = () => {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-  const [secondsLeft, setSecondsLeft] = useState(5);
+  const [secondsLeft, setSecondsLeft] = useState(10); // Durée augmentée à 10 secondes
   const [showResponse, setShowResponse] = useState(false);
   const [isPaused, setIsPaused] = useState(false);
   const [isPlaying, setIsPlaying] = useState(true);
@@ -28,10 +28,10 @@ const App = () => {
       setShowResponse(true);
       timeout = setTimeout(() => {
         setShowResponse(false);
-        setCurrentQuestionIndex((prevIndex) => 
+        setCurrentQuestionIndex((prevIndex) =>
           prevIndex < questions.length - 1 ? prevIndex + 1 : 0
         );
-        setSecondsLeft(5);
+        setSecondsLeft(10); // Réinitialisation à 10 secondes
       }, responseTime);
     }
 
@@ -53,40 +53,46 @@ const App = () => {
 
   useEffect(() => {
     initializeDragAndDrop();
-}, []);
+  }, []);
 
   const currentQuestion: Question = questions[currentQuestionIndex];
 
-  
-    return (
-      <div className="App">
-        <div className="cube-handle">
-       <div className="cube">
-    <div className="face front"></div>
-    <div className="face back"></div>
-    <div className="face right"></div>
-    <div className="face left"></div>
-    <div className="face top"></div>
-    <div className="face bottom"></div>
-    </div>
-</div>
-
-        {isPlaying 
-          ? <button className="pause-button" onClick={handlePauseClick}><FaPause size={30} /></button>
-          : <button className="play-button" onClick={handlePlayClick}><FaPlay size={30} /></button>
-        }
-        <Card 
-          question={currentQuestion.question} 
-          reponses={currentQuestion.reponses} 
-          bonneReponse={currentQuestion.bonneReponse}
-          showResponse={showResponse}
-        />
-        {!showResponse && (
-          <ProgressBar value={secondsLeft} maxValue={5} />
-        )}
-        <TestResults/> 
+  return (
+    <div className="App">
+      <div className="cube-handle">
+        <div className="cube">
+          <div className="face front">
+            <div className="blob"></div>
+          </div>
+          <div className="face back">
+            <div className="blob"></div>
+          </div>
+          <div className="face right"></div>
+          <div className="face left"></div>
+          <div className="face top"></div>
+          <div className="face bottom"></div>
+        </div>
       </div>
-    );
+
+      {isPlaying ? (
+        <button className="pause-button" onClick={handlePauseClick}>
+          <FaPause size={30} />
+        </button>
+      ) : (
+        <button className="play-button" onClick={handlePlayClick}>
+          <FaPlay size={30} />
+        </button>
+      )}
+      <Card
+        question={currentQuestion.question}
+        reponses={currentQuestion.reponses}
+        bonneReponse={currentQuestion.bonneReponse}
+        showResponse={showResponse}
+      />
+      {!showResponse && <ProgressBar value={secondsLeft} maxValue={10} />}
+      <TestResults />
+    </div>
+  );
 };
 
 export default App;
