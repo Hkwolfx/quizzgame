788b9ebb40eb10739a96b57dd0619d99ff940080
fantasyalebar <bardotalexandre@gmail.com>
2024-02-09 17:00:12 +0100

    dataset

--- src/dataset.ts
+++ src/dataset.ts
@@ -9,363 +9,123 @@ export interface Question {
 export const questions: Question[] = [
   {
     question: "Quelle est la capitale de la France?",
-    reponses: { a: " réponse A -> Lyon", b: "réponse B -> Marseille", c: "réponse C -> Paris", d: "réponse D -> Nice" },
+    reponses: { A: " réponse A -> Lyon", B: "réponse B -> Marseille", C: "réponse C -> Paris", D: "réponse D -> Nice" },
     bonneReponse: "C",
   },
   {
-    question: "Qui est l'auteur du livre 1984 ?",
-    reponses: {
-      a: "George Orwell",
-      b: "Aldous Huxley",
-      c: "William Shakespeare",
-      d: "Mark Twain",
-    },
-    bonneReponse: "A",
-  },
-  {
-    question: "Quel est le plus grand océan?",
-    reponses: { a: "Atlantique", b: "Indien", c: "Pacifique", d: "Arctique" },
-    bonneReponse: "C",
-  },
-  {
-    question: "Combien de planètes dans le système solaire?",
-    reponses: { a: "7", b: "8", c: "9", d: "10" },
+    question: "Qui a écrit 'Le Petit Prince'?",
+    reponses: { A: "réponse A -> Victor Hugo", B: "réponse B -> Antoine de Saint-Exupéry", C: "réponse C -> Marcel Proust", D: "réponse D -> Émile Zola" },
     bonneReponse: "B",
   },
   {
-    question: "Combien de continents dans le monde?",
-    reponses: { a: "4", b: "5", c: "6", d: "7" },
-    bonneReponse: "D",
-  },
-  {
-    question: "Capitale de la France?",
-    reponses: { a: "Lyon", b: "Marseille", c: "Paris", d: "Nice" },
+    question: "Quel élément chimique a pour symbole 'O'?",
+    reponses: { A: "réponse A -> Or", B: "réponse B -> Osmium", C: "réponse C -> Oxygène", D: "réponse D -> Oganesson" },
     bonneReponse: "C",
   },
   {
-    question: "Symbole chimique de l'Oxygène?",
-    reponses: { a: "Or", b: "Osmium", c: "Oxygène", d: "Oganesson" },
+    question: "En quelle année l'homme a-t-il marché sur la Lune?",
+    reponses: { A: "réponse A -> 1969", B: "réponse B -> 1972", C: "réponse C -> 1958", D: "réponse D -> 1984" },
+    bonneReponse: "A",
+  },
+  {
+    question: "Quel est le plus long fleuve du monde?",
+    reponses: { A: "réponse A -> Le Nil", B: "réponse B -> L'Amazone", C: "réponse C -> Le Yangtsé", D: "réponse D -> Le Mississippi" },
+    bonneReponse: "B",
+  },
+  {
+    question: "Qui a peint 'La Nuit étoilée'?",
+    reponses: { A: "réponse A -> Pablo Picasso", B: "réponse B -> Leonardo da Vinci", C: "réponse C -> Vincent van Gogh", D: "réponse D -> Claude Monet" },
+    bonneReponse: "C",
+  },
+  {
+    question: "Qui est l'auteur de la série de livres 'Harry Potter'?",
+    reponses: { A: "réponse A -> J.K. Rowling", B: "réponse B -> Stephen King", C: "réponse C -> J.R.R. Tolkien", D: "réponse D -> George R.R. Martin" },
+    bonneReponse: "A",
+  },
+  {
+    question: "Quel est le plus grand océan du monde?",
+    reponses: { A: "réponse A -> Atlantique", B: "réponse B -> Indien", C: "réponse C -> Arctique", D: "réponse D -> Pacifique" },
     bonneReponse: "D",
   },
   {
-    question: "Kyoto est dans quel pays?",
-    reponses: { a: "Chine", b: "Japon", c: "Corée du Sud", d: "Thaïlande" },
-    bonneReponse: "b",
+    question: "Dans quel pays se trouve la ville de Dubaï?",
+    reponses: { A: "réponse A -> Arabie Saoudite", B: "réponse B -> Qatar", C: "réponse C -> Émirats Arabes Unis", D: "réponse D -> Oman" },
+    bonneReponse: "C",
   },
   {
-    question: "Peintre de La Nuit étoilée?",
-    reponses: {
-      a: "Pablo Picasso",
-      b: "Leonardo Da Vinci",
-      c: "Vincent Van Gogh",
-      d: "Claude Monet",
-    },
-    bonneReponse: "c",
+    question: "Quelle est la langue officielle du Brésil?",
+    reponses: { A: "réponse A -> Espagnol", B: "réponse B -> Portugais", C: "réponse C -> Français", D: "réponse D -> Anglais" },
+    bonneReponse: "B",
   },
   {
-    question: "L'eau bout à quel degré?",
-    reponses: { a: "Vrai", b: "Faux" },
-    bonneReponse: "a",
+    question: "Quel compositeur a écrit la célèbre 5ème Symphonie?",
+    reponses: { A: "réponse A -> Wolfgang Amadeus Mozart", B: "réponse B -> Johann Sebastian Bach", C: "réponse C -> Ludwig van Beethoven", D: "réponse D -> Pyotr Ilyich Tchaikovsky" },
+    bonneReponse: "C",
   },
   {
-    question: "Quel est le plus long fleuve du monde ?",
-    reponses: {
-      a: "Le Nil",
-      b: "L'Amazone",
-      c: "Le Yangtsé",
-      d: "Le Mississippi"
-    },
-    bonneReponse: "b"
+    question: "Quel est le livre le plus vendu au monde après la Bible?",
+    reponses: { A: "réponse A -> Le Seigneur des Anneaux", B: "réponse B -> Le Petit Prince", C: "réponse C -> Harry Potter à l'école des sorciers", D: "réponse D -> Le Livre des Morts" },
+    bonneReponse: "B",
+  },
+  {
+    question: "Quelle ville est surnommée 'La Ville des Lumières'?",
+    reponses: { A: "réponse A -> New York", B: "réponse B -> Paris", C: "réponse C -> Tokyo", D: "réponse D -> Londres" },
+    bonneReponse: "B",
+  },
+  {
+    question: "Dans quel pays se trouve le Machu Picchu?",
+    reponses: { A: "réponse A -> Bolivie", B: "réponse B -> Pérou", C: "réponse C -> Colombie", D: "réponse D -> Équateur" },
+    bonneReponse: "B",
   },
   {
     question: "Qui a inventé l'ampoule électrique ?",
-    reponses: {
-      a: "Nikola Tesla",
-      b: "Thomas Edison",
-      c: "Alexander Graham Bell",
-      d: "Albert Einstein"
-    },
-    bonneReponse: "b"
+    reponses: { A: "réponse A -> Nikola Tesla", B: "réponse B -> Thomas Edison", C: "réponse C -> Alexander Graham Bell", D: "réponse D -> Albert Einstein" },
+    bonneReponse: "B",
   },
   {
-    question: "Quelle planète est connue comme la 'Planète Rouge' ?",
-    reponses: {
-      a: "Mars",
-      b: "Vénus",
-      c: "Jupiter",
-      d: "Saturne"
-    },
-    bonneReponse: "a"
+    question: "Quelle est la monnaie officielle du Japon?",
+    reponses: { A: "réponse A -> Yen", B: "réponse B -> Won", C: "réponse C -> Yuan", D: "réponse D -> Dollar" },
+    bonneReponse: "A",
   },
   {
-    question: "Dans quel pays se trouve la ville de Venise ?",
-    reponses: {
-      a: "France",
-      b: "Espagne",
-      c: "Italie",
-      d: "Portugal"
-    },
-    bonneReponse: "c"
+    question: "Quel pays est connu comme le pays du Matin Calme?",
+    reponses: { A: "réponse A -> Chine", B: "réponse B -> Japon", C: "réponse C -> Corée du Sud", D: "réponse D -> Vietnam" },
+    bonneReponse: "C",
   },
   {
-    question: "Qui est l'auteur de 'La Joconde' ?",
-    reponses: {
-      a: "Vincent Van Gogh",
-      b: "Pablo Picasso",
-      c: "Leonardo da Vinci",
-      d: "Claude Monet"
-    },
-    bonneReponse: "c"
+    question: "Quel est le plus grand animal terrestre?",
+    reponses: { A: "réponse A -> Girafe", B: "réponse B -> Éléphant d'Afrique", C: "réponse C -> Rhinocéros", D: "réponse D -> Hippopotame" },
+    bonneReponse: "B",
   },
   {
-    question: "Quel est l'élément chimique avec le symbole 'H' ?",
-    reponses: {
-      a: "Oxygène",
-      b: "Hydrogène",
-      c: "Hélium",
-      d: "Azote"
-    },
-    bonneReponse: "b"
+    question: "Qui a écrit '1984'?",
+    reponses: { A: "réponse A -> George Orwell", B: "réponse B -> Aldous Huxley", C: "réponse C -> Arthur C. Clarke", D: "réponse D -> Ray Bradbury" },
+    bonneReponse: "A",
   },
   {
-    question: "Quelle est la capitale du Canada ?",
-    reponses: {
-      a: "Toronto",
-      b: "Montreal",
-      c: "Vancouver",
-      d: "Ottawa"
-    },
-    bonneReponse: "d"
+    question: "Quelle ville est la capitale de l'Australie?",
+    reponses: { A: "réponse A -> Sydney", B: "réponse B -> Melbourne", C: "réponse C -> Canberra", D: "réponse D -> Perth" },
+    bonneReponse: "C",
   },
   {
-    question: "Qui a composé les Quatre Saisons ?",
-    reponses: {
-      a: "Ludwig van Beethoven",
-      b: "Antonio Vivaldi",
-      c: "Johann Sebastian Bach",
-      d: "Wolfgang Amadeus Mozart"
-    },
-    bonneReponse: "b"
+    question: "Quel artiste est célèbre pour avoir coupé son oreille ?",
+    reponses: { A: "réponse A -> Pablo Picasso", B: "réponse B -> Vincent van Gogh", C: "réponse C -> Salvador Dalí", D: "réponse D -> Claude Monet" },
+    bonneReponse: "B",
   },
   {
-    question: "Quel pays a pour capitale Berlin ?",
-    reponses: {
-      a: "France",
-      b: "Allemagne",
-      c: "Autriche",
-      d: "Belgique"
-    },
-    bonneReponse: "b"
+    question: "Quel est le plus petit pays du monde?",
+    reponses: { A: "réponse A -> Monaco", B: "réponse B -> Liechtenstein", C: "réponse C -> Vatican", D: "réponse D -> Nauru" },
+    bonneReponse: "C",
   },
   {
-    question: "Quelle est la plus grande planète du système solaire ?",
-    reponses: {
-      a: "Saturne",
-      b: "Jupiter",
-      c: "Mars",
-      d: "Terre"
-    },
-    bonneReponse: "b"
+    question: "Qui est le dieu de la guerre dans la mythologie grecque?",
+    reponses: { A: "réponse A -> Zeus", B: "réponse B -> Arès", C: "réponse C -> Apollon", D: "réponse D -> Hermès" },
+    bonneReponse: "B",
   },
   {
-    question: "Quelle est la capitale du Canada ?",
-    reponses: {
-      a: "Toronto",
-      b: "Montreal",
-      c: "Vancouver",
-      d: "Ottawa"
-    },
-    bonneReponse: "d"
-  },
-  {
-    question: "Qui a composé les Quatre Saisons ?",
-    reponses: {
-      a: "Ludwig van Beethoven",
-      b: "Antonio Vivaldi",
-      c: "Johann Sebastian Bach",
-      d: "Wolfgang Amadeus Mozart"
-    },
-    bonneReponse: "b"
-  },
-  {
-    question: "Quel pays a pour capitale Berlin ?",
-    reponses: {
-      a: "France",
-      b: "Allemagne",
-      c: "Autriche",
-      d: "Belgique"
-    },
-    bonneReponse: "b"
-  },
-  {
-    question: "Quelle est la plus grande planète du système solaire ?",
-    reponses: {
-      a: "Saturne",
-      b: "Jupiter",
-      c: "Mars",
-      d: "Terre"
-    },
-    bonneReponse: "b"
-  },
-  {
-    question: "Quel pays est connu comme le pays du Soleil-Levant ?",
-    reponses: {
-      a: "Inde",
-      b: "Égypte",
-      c: "Japon",
-      d: "Australie"
-    },
-    bonneReponse: "c"
-  },
-  {
-    question: "Qui a écrit 'L'Ingénu' ?",
-    reponses: {
-      a: "Voltaire",
-      b: "Molière",
-      c: "Victor Hugo",
-      d: "Émile Zola"
-    },
-    bonneReponse: "a"
-  },
-  {
-    question: "Dans quelle ville peut-on visiter la Tour Eiffel ?",
-    reponses: {
-      a: "Londres",
-      b: "Berlin",
-      c: "Paris",
-      d: "Bruxelles"
-    },
-    bonneReponse: "c"
-  },
-  {
-    question: "Quel est le plus long fleuve du monde ?",
-    reponses: {
-      a: "Nil",
-      b: "Amazone",
-      c: "Yangtsé",
-      d: "Mississippi"
-    },
-    bonneReponse: "b"
-  },
-  {
-    question: "Quel compositeur est célèbre pour la 'Lettre à Élise' ?",
-    reponses: {
-      a: "Frédéric Chopin",
-      b: "Ludwig van Beethoven",
-      c: "Wolfgang Amadeus Mozart",
-      d: "Johann Sebastian Bach"
-    },
-    bonneReponse: "b"
-  },
-  {
-    question: "Qui a peint 'La Nuit étoilée' ?",
-    reponses: {
-      a: "Pablo Picasso",
-      b: "Leonardo da Vinci",
-      c: "Vincent van Gogh",
-      d: "Claude Monet"
-    },
-    bonneReponse: "c"
-  },
-  {
-    question: "Quel est l'océan le plus profond du monde ?",
-    reponses: {
-      a: "Océan Atlantique",
-      b: "Océan Indien",
-      c: "Océan Arctique",
-      d: "Océan Pacifique"
-    },
-    bonneReponse: "d"
-  },
-  {
-    question: "Quelle structure est considérée comme la plus ancienne des sept merveilles du monde ?",
-    reponses: {
-      a: "La Statue de Zeus à Olympie",
-      b: "Le Colosse de Rhodes",
-      c: "La Grande Pyramide de Gizeh",
-      d: "Le Phare d'Alexandrie"
-    },
-    bonneReponse: "c"
-  },
-  {
-    question: "Quel dirigeant est connu pour avoir été le fondateur de l'Empire mongol ?",
-    reponses: {
-      a: "Gengis Khan",
-      b: "Kubilai Khan",
-      c: "Tamerlan",
-      d: "Attila"
-    },
-    bonneReponse: "a"
-  },
-  {
-    question: "Quelle est la capitale de la Nouvelle-Zélande ?",
-    reponses: {
-      a: "Auckland",
-      b: "Christchurch",
-      c: "Wellington",
-      d: "Hamilton"
-    },
-    bonneReponse: "c"
-  },
-  {
-    question: "Qui a écrit 'À la recherche du temps perdu' ?",
-    reponses: {
-      a: "Marcel Proust",
-      b: "Victor Hugo",
-      c: "Gustave Flaubert",
-      d: "Honoré de Balzac"
-    },
-    bonneReponse: "a"
-  },
-  {
-    question: "Quel élément a pour numéro atomique 79 ?",
-    reponses: {
-      a: "Fer",
-      b: "Or",
-      c: "Argent",
-      d: "Plomb"
-    },
-    bonneReponse: "b"
-  },
-  {
-    question: "Quelle ville est surnommée 'La Ville Lumière' ?",
-    reponses: {
-      a: "New York",
-      b: "Paris",
-      c: "Londres",
-      d: "Tokyo"
-    },
-    bonneReponse: "b"
-  },
-  {
-    question: "Qui est considéré comme le père de l'informatique ?",
-    reponses: {
-      a: "Charles Babbage",
-      b: "Alan Turing",
-      c: "Bill Gates",
-      d: "Steve Jobs"
-    },
-    bonneReponse: "a"
-  },
-  {
-    question: "Quel est le plus grand animal terrestre ?",
-    reponses: {
-      a: "Le rhinocéros blanc",
-      b: "L'éléphant d'Afrique",
-      c: "Le gorille",
-      d: "Le tigre de Sibérie"
-    },
-    bonneReponse: "b"
-  },
-  {
-    question: "Dans quel pays se trouve le Taj Mahal ?",
-    reponses: {
-      a: "Inde",
-      b: "Pakistan",
-      c: "Bangladesh",
-      d: "Népal"
-    },
-    bonneReponse: "a"
+    question: "Quel film a remporté l'Oscar du meilleur film en 1994?",
+    reponses: { A: "réponse A -> Pulp Fiction", B: "réponse B -> Forrest Gump", C: "réponse C -> Le Roi Lion", D: "réponse D -> Shawshank Redemption" },
+    bonneReponse: "B",
   }
   // Ajoutez d'autres questions ici
 ];
