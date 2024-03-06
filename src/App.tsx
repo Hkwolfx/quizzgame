ec056b24aeb584a5aa406754adc358cddaa5c6e1
fantasyalebar <bardotalexandre@gmail.com>
2024-03-06 14:25:51 +0100

    Add new image files and update test results

--- src/App.tsx
+++ src/App.tsx
@@ -23,9 +23,8 @@ interface Notification {
   likeCount?: number; // pour les likes
   action?: string; // pour les actions sociales
   giftId?: string; // pour les cadeaux
-  type: 'like' | 'social' | 'gift'; // distingue le type de notification
+  type: "like" | "social" | "gift"; // distingue le type de notification
 }
-
 
 const App = () => {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
@@ -38,7 +37,9 @@ const App = () => {
   const [revealAnswers, setRevealAnswers] = useState(false); // Cet état contrôle l'affichage des options de réponse
   const [startSent, setStartSent] = useState(false); // Ajouté pour contrôler l'envoi de la requête
   const [meilleursJoueurs, setMeilleursJoueurs] = useState<Joueur[]>([]);
-  const [utilisateursNotifies, setUtilisateursNotifies] = useState<Notification[]>([]);
+  const [utilisateursNotifies, setUtilisateursNotifies] = useState<
+    Notification[]
+  >([]);
   const [currentUserIndex, setCurrentUserIndex] = useState(0);
   const [showThankYou, setShowThankYou] = useState(false);
 
@@ -55,7 +56,9 @@ const App = () => {
       setShowResponse(true);
       setTimeout(() => {
         setShowResponse(false);
-        setCurrentQuestionIndex((prevIndex) => prevIndex < questions.length - 1 ? prevIndex + 1 : 0);
+        setCurrentQuestionIndex((prevIndex) =>
+          prevIndex < questions.length - 1 ? prevIndex + 1 : 0
+        );
         setSecondsLeft(30); // Réinitialisation pour la prochaine question
         setStartSent(false); // Réinitialisation de l'envoi de la requête pour la prochaine question
       }, responseTime);
@@ -66,69 +69,85 @@ const App = () => {
     if (secondsLeft === 30 && !startSent && !showResponse) {
       setStartSent(true); // Pour éviter les envois multiples
       setRevealAnswers(false); // Commencer par cacher les réponses
-  
+
       // Révéler les réponses 1 seconde avant d'envoyer la requête /start
       setTimeout(() => {
         setRevealAnswers(true);
       }, 2000); // Attendre 2 secondes pour révéler les réponses, donc 1 seconde avant la requête /start
-  
+
       // Ajout d'un délai total de 3 secondes avant d'exécuter l'envoi des données /start
       setTimeout(() => {
         const currentQuestionData = {
           question: currentQuestionIndex,
           bonneReponse: questions[currentQuestionIndex].bonneReponse,
         };
-  
-        console.log('Envoi des données au serveur pour la question:', currentQuestionData.question);
-  
-        fetch('http://localhost:3000/start', {
-          method: 'POST',
-          headers: {'Content-Type': 'application/json'},
+
+        console.log(
+          "Envoi des données au serveur pour la question:",
+          currentQuestionData.question
+        );
+
+        fetch("http://localhost:3000/start", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
           body: JSON.stringify(currentQuestionData),
         })
-        .then(response => response.ok ? response.json() : Promise.reject('Erreur réseau'))
-        .then(data => {
-          setIsAudioPlayed(data.audioPlayed);
-          // Ici, vous pouvez mettre à jour les états nécessaires après la réponse /start
-        })
-        .catch(error => console.error('Erreur lors de la requête:', error));
+          .then((response) =>
+            response.ok ? response.json() : Promise.reject("Erreur réseau")
+          )
+          .then((data) => {
+            setIsAudioPlayed(data.audioPlayed);
+            // Ici, vous pouvez mettre à jour les états nécessaires après la réponse /start
+          })
+          .catch((error) => console.error("Erreur lors de la requête:", error));
       }, 3000); // 3000 millisecondes = 3 secondes pour l'envoi de la requête /start
     }
   }, [currentQuestionIndex, secondsLeft, showResponse, startSent]);
-  
 
   useEffect(() => {
     if (showResponse) {
-      fetch('http://localhost:3000/end', {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({ bonneReponse: questions[currentQuestionIndex].bonneReponse }),
+      fetch("http://localhost:3000/end", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          bonneReponse: questions[currentQuestionIndex].bonneReponse,
+        }),
       })
-      .then(response => response.ok ? response.json() : Promise.reject('Erreur réseau'))
-      .then(data => {
-        setMeilleursJoueurs(prevMeilleursJoueurs => {
-          const updatedScoresMap = new Map();
-  
-          data.meilleursJoueurs.forEach((joueurFromServer: { userId: any; }) => {
-            updatedScoresMap.set(joueurFromServer.userId, joueurFromServer);
+        .then((response) =>
+          response.ok ? response.json() : Promise.reject("Erreur réseau")
+        )
+        .then((data) => {
+          setMeilleursJoueurs((prevMeilleursJoueurs) => {
+            const updatedScoresMap = new Map();
+
+            data.meilleursJoueurs.forEach(
+              (joueurFromServer: { userId: any }) => {
+                updatedScoresMap.set(joueurFromServer.userId, joueurFromServer);
+              }
+            );
+
+            const mergedScores = prevMeilleursJoueurs.map(
+              (joueur) => updatedScoresMap.get(joueur.userId) || joueur
+            );
+            const uniqueMergedScores = Array.from(
+              new Set([...mergedScores, ...data.meilleursJoueurs])
+            );
+
+            return uniqueMergedScores
+              .sort((a, b) => b.score - a.score)
+              .slice(0, 3); // Assurez-vous de ne garder que les trois meilleurs joueurs
           });
-  
-          const mergedScores = prevMeilleursJoueurs.map(joueur => updatedScoresMap.get(joueur.userId) || joueur);
-          const uniqueMergedScores = Array.from(new Set([...mergedScores, ...data.meilleursJoueurs]));
-          
-          return uniqueMergedScores
-            .sort((a, b) => b.score - a.score)
-            .slice(0, 3); // Assurez-vous de ne garder que les trois meilleurs joueurs
-        });
-  
-        console.log('Mise à jour des meilleurs joueurs avec les nouveaux scores.');
-      })
-      .catch(error => console.error('Erreur lors de la requête /end:', error));
+
+          console.log(
+            "Mise à jour des meilleurs joueurs avec les nouveaux scores."
+          );
+        })
+        .catch((error) =>
+          console.error("Erreur lors de la requête /end:", error)
+        );
     }
   }, [showResponse, currentQuestionIndex, questions]);
 
-
-  
   // Boutons de contrôle
   const handlePlayClick = () => {
     setIsPlaying(true);
@@ -150,18 +169,32 @@ const App = () => {
 
   const fetchUtilisateursNotifies = () => {
     Promise.all([
-      fetch('http://localhost:3000/likes-notifies').then(res => res.json()),
-      fetch('http://localhost:3000/socials-notifies').then(res => res.json()),
-      fetch('http://localhost:3000/gifts-notifies').then(res => res.json())
-    ]).then(([likesData, socialsData, cadeauxData]) => {
-      // Combinez toutes les notifications en une liste, en ajoutant un champ pour indiquer le type
-      const combinedData = [
-        ...likesData.map((item : Notification) => ({ ...item, type: 'like' } as Notification)),
-        ...socialsData.map((item : Notification) => ({ ...item, type: 'social' } as Notification)),
-        ...cadeauxData.map((item : Notification) => ({ ...item, type: 'gift' } as Notification))
-      ];
-      setUtilisateursNotifies(combinedData);
-    }).catch(error => console.error('Erreur lors de la récupération des notifications:', error));
+      fetch("http://localhost:3000/likes-notifies").then((res) => res.json()),
+      fetch("http://localhost:3000/socials-notifies").then((res) => res.json()),
+      fetch("http://localhost:3000/gifts-notifies").then((res) => res.json()),
+    ])
+      .then(([likesData, socialsData, cadeauxData]) => {
+        // Combinez toutes les notifications en une liste, en ajoutant un champ pour indiquer le type
+        const combinedData = [
+          ...likesData.map(
+            (item: Notification) => ({ ...item, type: "like" } as Notification)
+          ),
+          ...socialsData.map(
+            (item: Notification) =>
+              ({ ...item, type: "social" } as Notification)
+          ),
+          ...cadeauxData.map(
+            (item: Notification) => ({ ...item, type: "gift" } as Notification)
+          ),
+        ];
+        setUtilisateursNotifies(combinedData);
+      })
+      .catch((error) =>
+        console.error(
+          "Erreur lors de la récupération des notifications:",
+          error
+        )
+      );
   };
 
   useEffect(() => {
@@ -171,7 +204,10 @@ const App = () => {
 
   useEffect(() => {
     // On affiche un utilisateur si il y en a dans la liste et que l'index courant est inférieur à la longueur de la liste
-    if (utilisateursNotifies.length > 0 && currentUserIndex < utilisateursNotifies.length) {
+    if (
+      utilisateursNotifies.length > 0 &&
+      currentUserIndex < utilisateursNotifies.length
+    ) {
       setShowThankYou(true);
       const timer = setTimeout(() => {
         setShowThankYou(false);
@@ -186,7 +222,6 @@ const App = () => {
       setShowThankYou(false);
     }
   }, [currentUserIndex, utilisateursNotifies]);
-  
 
   return (
     <div className="App">
@@ -199,26 +234,46 @@ const App = () => {
           <FaPlay size={30} />
         </button>
       )}
-       {showThankYou && currentUserIndex < utilisateursNotifies.length && (
-    <div className="thank-you-messages">
-      <div className="star star1">★</div>
-      <div className="star star2">★</div>
-      <div className="star star3">★</div>
-      <div className="star star4">★</div>
-      <div>
-      Merci, {utilisateursNotifies[currentUserIndex].nickname} pour {utilisateursNotifies[currentUserIndex].type === 'like' ? `les likes` : utilisateursNotifies[currentUserIndex].type === 'social' ? `l'action sociale` : `le cadeau ${utilisateursNotifies[currentUserIndex].giftId ? `ID: ${utilisateursNotifies[currentUserIndex].giftId}` : ''}`}      </div>
-    </div>
-  )}
-      <div style={{ visibility: isCardVisible ? "visible" : "hidden" }} >
-      <Card
-        question={currentQuestion.question}
-        reponses={currentQuestion.reponses}
-        bonneReponse={currentQuestion.bonneReponse}
-        showResponse={showResponse}
-        revealAnswers={revealAnswers}
-      />
+      {showThankYou && currentUserIndex < utilisateursNotifies.length && (
+        <div className="thank-you-messages">
+          <div className="star star1">★</div>
+          <div className="star star2">★</div>
+          <div className="star star3">★</div>
+          <div className="star star4">★</div>
+          <div>
+            Merci, {utilisateursNotifies[currentUserIndex].nickname} pour{" "}
+            {utilisateursNotifies[currentUserIndex].type === "like"
+              ? `les likes`
+              : utilisateursNotifies[currentUserIndex].type === "social"
+              ? `l'action sociale`
+              : `le cadeau ${
+                  utilisateursNotifies[currentUserIndex].giftId
+                    ? `ID: ${utilisateursNotifies[currentUserIndex].giftId}`
+                    : ""
+                }`}{" "}
+          </div>
+        </div>
+      )}
+      <div style={{ visibility: isCardVisible ? "visible" : "hidden" }}>
+        <Card
+          question={currentQuestion.question}
+          reponses={currentQuestion.reponses}
+          bonneReponse={currentQuestion.bonneReponse}
+          showResponse={showResponse}
+          revealAnswers={revealAnswers}
+        />
       </div>
+
       <Ranking meilleursJoueurs={meilleursJoueurs} />
+      <div className="wrap">
+        <div className="x-container">
+          <div className="group">
+            <div className="icon"><div className="plusone">+1</div></div>
+            <div className="icon1"><div className="plusone">+1</div></div>
+            <div className="icon2"><div className="plusone">+1</div></div>
+          </div>
+        </div>
+      </div>
       {!showResponse && <ProgressBar value={secondsLeft} maxValue={10} />}
       <TestResults />
     </div>
